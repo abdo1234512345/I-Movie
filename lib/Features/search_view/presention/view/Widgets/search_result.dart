c3ef2462e1aa83a338072203874b3b1bf3f58cf0
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-15 22:41:48 +0200

    Create Search Result To show Result of Search

--- lib/Features/search_view/presention/view/Widgets/search_result.dart
+++ lib/Features/search_view/presention/view/Widgets/search_result.dart
@@ -12,29 +12,28 @@ class SearchResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
-        itemCount: _results.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(vertical: 8),
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16),
-              leading: _results[index]['poster_path'] != null
-                  ? Image.network(
-                      'https://image.tmdb.org/t/p/w500${_results[index]['poster_path']}',
-                      width: 120,
-                      height: 160,
-                      fit: BoxFit.cover,
-                    )
-                  : NOImage(),
-              title: Text(
-                _results[index]['title'] ?? 'No Title',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+        child: ListView.builder(
+      itemCount: _results.length,
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          child: ListTile(
+            contentPadding: EdgeInsets.all(16),
+            leading: _results[index]['poster_path'] != null
+                ? Image.network(
+                    'https://image.tmdb.org/t/p/w500${_results[index]['poster_path']}',
+                    width: 120,
+                    height: 160,
+                    fit: BoxFit.cover,
+                  )
+                : NOImage(),
+            title: Text(
+              _results[index]['title'] ?? 'No Title',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          );
-        },
-      ),
-    );
+          ),
+        );
+      },
+    ));
   }
 }
